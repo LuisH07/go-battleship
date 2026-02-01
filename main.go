1798414626440da630fbcd50165da5ebb44fdbc7
cauaof_souza <cauasantos064@gmail.com>
2026-02-01 01:42:48 -0300

    adicionando game com demonstrativos e main preparada para testes de front
    há configurações do jogo na main sugeridas por mim, além do setup de text e testes demonstrando uso dos componentes feitos (init1 e init2 em game)

--- main.go
+++ main.go
@@ -1,38 +1,18 @@
 package main
+
 import (
-	"github.com/allanjose001/go-battleship/UI"
 	"github.com/allanjose001/go-battleship/game"
-	"fmt"
+	"github.com/allanjose001/go-battleship/game/components"
+	"github.com/hajimehoshi/ebiten/v2"
 )
 
 func main() {
-	fmt.Println("Backend is running...");
-	
-	ui.Run();
-
-	board1 := new(game.Board);
-
-	game.PrintBoard(board1);
-
-	barco1 := new(game.Ship);
-	barco1.Size = 3;
-	barco1.Horizontal = true;
-
-	barco2 := new(game.Ship);
-	barco2.Size = 3;
-
-	fmt.Println("");
-
-	game.PlaceShip(board1, barco1, 1, 1);
-
-	game.PrintBoard(board1);
-
-	fmt.Println("");
-
-	fmt.Println("hit count barco1:", barco1.HitCount);
-
-	game.AttackPosition(board1, 1, 1);
-
-	fmt.Println("hit count barco1:", barco1.HitCount);
+	components.InitFonts() //carrega a fonte apenas uma vez
+	g := game.NewGame()
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	err := ebiten.RunGame(g)
+	if err != nil {
+		panic(err)
+	}
 
 }
